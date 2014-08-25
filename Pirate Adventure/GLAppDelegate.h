199b6cd61229a391dca5572daeeb443e2b103983
Millin Gabani <gabanimillin@gmail.com>
2014-08-25 12:46:46 -0400

    Initial Commit

---
+++ Pirate Adventure/GLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GLAppDelegate.h
+//  Pirate Adventure
+//
+//  Created by Gab on 2014-08-25.
+//  Copyright (c) 2014 Gab Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pirate Adventure/GLViewController.h
@@ -0,0 +1,13 @@
+//
+//  GLViewController.h
+//  Pirate Adventure
+//
+//  Created by Gab on 2014-08-25.
+//  Copyright (c) 2014 Gab Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GLViewController : UIViewController
+
+@end
